ce25f2c499a4411d567afe0757ef5576ab0c637d
Lucy Knight <lucy@jargonautical.com>
2020-04-09 11:24:23 +0100

    Create scroll-ip-address.py

---
+++ senseHAT/scroll-ip-address.py
@@ -0,0 +1,18 @@
+import socket
+import fcntl
+import struct
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+# print (get_ip_address('wlan0'))
+my_IP = get_ip_address('wlan0')
+
+from sense_hat import SenseHat
+sense = SenseHat()
+sense.show_message("IP address is %s" % my_IP) 
