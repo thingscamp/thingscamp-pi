8812dc327addca6d96ec4b741d3d135329ffaa92
Lucy Knight <lucy@jargonautical.com>
2020-04-09 11:22:55 +0100

    Create make-another-barchart.py

---
+++ minecraft-pi/make-another-barchart.py
@@ -0,0 +1,26 @@
+# import minecraft libraries
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+mc = minecraft.Minecraft.create()
+
+# get player position
+pPos = mc.player.getTilePos()
+print pPos
+
+# post a message to chat
+mc.postToChat("Have some data ...")
+# set the data array
+data = (3,2,4,1)
+mc.postToChat(data)
+mc.postToChat(str(len(data))+" values")
+
+
+# clear a space and create a baseplate
+# mc.setBlocks(pPos.x-5,pPos.y+20,pPos.z-5,pPos.x+5,pPos.y,pPos.z+10,block.AIR)
+# mc.setBlocks(pPos.x-5,pPos.y,pPos.z-5,pPos.x+5,pPos.y,pPos.z+10,block.SNOW_BLOCK)
+ 
+# add blocks for data
+mc.setBlocks(pPos.x+1,pPos.y,pPos.z+1,pPos.x+1,pPos.y+data[0],pPos.z+1,block.DIAMOND_BLOCK)
+mc.setBlocks(pPos.x+1,pPos.y,pPos.z+2,pPos.x+1,pPos.y+data[1],pPos.z+2,block.GOLD_BLOCK)
+mc.setBlocks(pPos.x+1,pPos.y,pPos.z+3,pPos.x+1,pPos.y+data[2],pPos.z+3,block.IRON_BLOCK)
+mc.setBlocks(pPos.x+1,pPos.y,pPos.z+4,pPos.x+1,pPos.y+data[3],pPos.z+4,block.LEAVES)
